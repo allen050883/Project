5066020db3791634f6dd9212997614e69fa8f2e3
Tseng Po-Yen <allen050883@gmail.com>
2021-12-18 00:06:54 +0800

    Update config.py

--- kaggle_SIIM-ACR_Pneumothorax_Segmentation/utils/config.py
+++ kaggle_SIIM-ACR_Pneumothorax_Segmentation/utils/config.py
@@ -2,27 +2,28 @@ class Config:
     def __init__(self):
         # Deep learning hyperparameter
         self.TRAIN_TEST_SPLIT = 0.8
+        self.UPSAMPLEING = True
 
         # Epoch from START_EPOCH to END_EPOCH
         self.START_EPOCH = 1
         self.END_EPOCH = 500
 
         # Learning rate
-        self.LERANING_RATE = 0.001
+        self.LERANING_RATE = 1e-2
 
         # Batch size setting
-        self.BATCH_SIZE_TRAINING = 84
-        self.BATCH_SIZE_VALIDATION = 84
+        self.BATCH_SIZE_TRAINING = 72
+        self.BATCH_SIZE_VALIDATION = 72
         self.BATCH_SIZE_TESTING = 1
 
         # Save directory
         self.MODEL_SAVE_DIR = 'model_save'
-        self.CHECK_DATASET_DIR = 'check_dataset'
-        self.PRED_SAVE_DIR = 'pred_train_dice_score_0.7375_val_dice_score_0.6585'
+        self.PRED_SAVE_DIR = 'pred_model_epoch_356.0_train_dice_score_0.8079_val_dice_score_0.4811'
 
         # Model weight
-        self.PRETRAIN_WEIGHT = "./model_save_1208_256_2/model_epoch_433.0_train_dice_score_0.3575_val_dice_score_0.3625.pth"
-        self.INFERENCE_WEIGHT = "./model_save_1205/model_epoch_197.0_train_dice_score_0.7375_val_dice_score_0.6585.pth"
+        self.PRETRAIN_WEIGHT = ''
+        # self.PRETRAIN_WEIGHT = 'model_epoch_53.0_train_dice_score_0.9018_val_dice_score_0.9049.pth'
+        self.INFERENCE_WEIGHT = './model_save/model_epoch_356.0_train_dice_score_0.8079_val_dice_score_0.4811.pth'
 
         # File writer
         self.MODEL_SAVE_DIR = "model_save"
